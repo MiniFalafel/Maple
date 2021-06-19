4c7f8b5cdee33b193b93e71cafbf744e6f9d2823
MiniFalafel <67521442+MiniFalafel@users.noreply.github.com>
2021-06-19 13:03:34 -0400

    Create premake script

---
+++ premake5.lua
@@ -0,0 +1,97 @@
+workspace "Maple"
+	architecture "x64"
+	
+	configurations {
+		"Debug",
+		"Release",
+		"Dist"
+	}
+	
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+	
+project "Maple"
+	location "Maple"
+	kind "SharedLib"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include",
+		"%{prj.name}/src"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines {
+			"MP_PLATFORM_WINDOWS",
+			"MP_BUILD_DLL"
+		}
+		
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+		
+	filter "configurations:Debug"
+		defines "MP_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "MP_RELEASE"
+		optimize "On"
+	
+	filter "configurations:Dist"
+		defines "MP_DIST"
+		optimize "On"
+		
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs {
+		"Maple/vendor/spdlog/include",
+		"Maple/src"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines {
+			"MP_PLATFORM_WINDOWS",
+		}
+		
+		links {
+			"Maple"
+		}
+		
+	filter "configurations:Debug"
+		defines "MP_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "MP_RELEASE"
+		optimize "On"
+	
+	filter "configurations:Dist"
+		defines "MP_DIST"
+		optimize "On"
