f7479bfc990f14e6c53ba24a2a8c2c52e9eaa3f0
MiniFalafel <67521442+MiniFalafel@users.noreply.github.com>
2021-06-18 23:35:45 -0400

    Create SandboxApp.cpp

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,19 @@
+#include <Maple.h>
+
+class Sandbox : public Maple::Application {
+
+public:
+
+	Sandbox() {
+		
+	}
+
+	~Sandbox() {
+		
+	}
+
+};
+
+Maple::Application* Maple::CreateApplication() {
+	return new Sandbox();
+}
